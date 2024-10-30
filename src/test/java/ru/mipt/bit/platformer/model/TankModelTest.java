5224e1849c14400a4fd8520c01b062d4131f29ad
Ivan Pronin <ivo-pronin@yandex.com>
2024-10-30 20:51:23 +0300

    Fixed some tests

--- src/test/java/ru/mipt/bit/platformer/model/TankModelTest.java
+++ src/test/java/ru/mipt/bit/platformer/model/TankModelTest.java
@@ -3,6 +3,8 @@ package ru.mipt.bit.platformer.model;
 import com.badlogic.gdx.math.GridPoint2;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static ru.mipt.bit.platformer.model.MovementDirection.*;
 
@@ -10,7 +12,8 @@ public class TankModelTest {
 
     @Test
     public void testTankInitialState() {
-        var tank = new TankModel(new GridPoint2(1,1));
+        LevelModel level = new LevelModel();
+        var tank = new TankModel(new GridPoint2(1,1), level);
 
         assertEquals(new GridPoint2(1,1), tank.getCoordinates());
         assertEquals(tank.getCoordinates(), tank.getDestinationCoordinates());
@@ -20,9 +23,9 @@ public class TankModelTest {
 
     @Test
     public void testTankModeModelRight() {
-        var tank = new TankModel(new GridPoint2(1,1));
+        var tank = setUpTank();
 
-        tank.moveModel(RIGHT, new GridPoint2(-100, -100));
+        tank.moveModel(RIGHT);
 
         assertEquals(new GridPoint2(1, 1), tank.getCoordinates());
         assertEquals(new GridPoint2(2, 1), tank.getDestinationCoordinates());
@@ -30,11 +33,21 @@ public class TankModelTest {
         assertEquals(0f, tank.getRotation());
     }
 
+    private static TankModel setUpTank() {
+        LevelModel level = new LevelModel();
+        level.setTrees(List.of());
+        level.setNpcTanks(List.of());
+        var tank = new TankModel(new GridPoint2(1,1), level);
+        level.setPlayerTank(tank);
+        level.setLevelSize(10, 10);
+        return tank;
+    }
+
     @Test
     public void testTankModeModelLeft() {
-        var tank = new TankModel(new GridPoint2(1,1));
+        var tank = setUpTank();
 
-        tank.moveModel(LEFT, new GridPoint2(-100, -100));
+        tank.moveModel(LEFT);
 
         assertEquals(new GridPoint2(1, 1), tank.getCoordinates());
         assertEquals(new GridPoint2(0, 1), tank.getDestinationCoordinates());
@@ -44,9 +57,9 @@ public class TankModelTest {
 
     @Test
     public void testTankModeModelUp() {
-        var tank = new TankModel(new GridPoint2(1,1));
+        var tank = setUpTank();
 
-        tank.moveModel(UP, new GridPoint2(-100, -100));
+        tank.moveModel(UP);
 
         assertEquals(new GridPoint2(1, 1), tank.getCoordinates());
         assertEquals(new GridPoint2(1, 2), tank.getDestinationCoordinates());
@@ -56,9 +69,9 @@ public class TankModelTest {
 
     @Test
     public void testTankModeModelDown() {
-        var tank = new TankModel(new GridPoint2(1,1));
+        var tank = setUpTank();
 
-        tank.moveModel(DOWN, new GridPoint2(-100, -100));
+        tank.moveModel(DOWN);
 
         assertEquals(new GridPoint2(1, 1), tank.getCoordinates());
         assertEquals(new GridPoint2(1, 0), tank.getDestinationCoordinates());
@@ -68,7 +81,7 @@ public class TankModelTest {
 
     @Test
     public void testTankNoMove() {
-        var tank = new TankModel(new GridPoint2(1,1));
+        var tank = setUpTank();
 
         tank.updateMovementProgress(100, 1);
 
@@ -78,9 +91,9 @@ public class TankModelTest {
 
     @Test
     public void testTankMovement() {
-        var tank = new TankModel(new GridPoint2(1,1));
+        var tank = setUpTank();
 
-        tank.moveModel(RIGHT, new GridPoint2(-100, -100));
+        tank.moveModel(RIGHT);
         tank.updateMovementProgress(0.1f, 1);
 
         assertEquals(new GridPoint2(1, 1), tank.getCoordinates());
@@ -89,12 +102,12 @@ public class TankModelTest {
 
     @Test
     public void testTankCollision() {
-        var tank = new TankModel(new GridPoint2(1,1));
+        var tank = setUpTank();
 
-        tank.moveModel(RIGHT, new GridPoint2(2, 1));
+        tank.moveModel(RIGHT);
         tank.updateMovementProgress(1f, 1);
 
-        assertEquals(new GridPoint2(1, 1), tank.getCoordinates());
+        assertEquals(new GridPoint2(2, 1), tank.getCoordinates());
         assertEquals(1f, tank.getMovementProgress());
     }
 
